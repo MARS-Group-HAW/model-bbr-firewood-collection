23ee1aa79878362080bdeed920a1b5a9c2e22d76
Florian J. Ocker <florian.ocker@haw-hamburg.de>
2019-02-05 17:06:56 +0100

    FirewoodCollector -deleted old trees

--- Starter.cs
+++ Starter.cs
@@ -1,25 +1,31 @@
 ﻿using System;
-using System.IO;
-using Accord.Math;
+using System.Diagnostics;
+using System.Linq;
+using Bushbuckridge.Agents.Collector;
+using KruegerNationalPark;
+using Mars.Common.Logging;
+using Mars.Common.Logging.Enums;
 using Mars.Core.ModelContainer.Entities;
+using Mars.Core.SimulationStarter;
 
 public static class Program {
     public static void Main(string[] args) {
-        if (args != null && System.Linq.Enumerable.Any(args, s => s.Equals("-l")))
+        if (args != null && Enumerable.Any(args, s => s.Equals("-l")))
         {
-            Mars.Common.Logging.LoggerFactory.SetLogLevel(Mars.Common.Logging.Enums.LogLevel.Info);
-            Mars.Common.Logging.LoggerFactory.ActivateConsoleLogging();
+            LoggerFactory.SetLogLevel(LogLevel.Info);
+            LoggerFactory.ActivateConsoleLogging();
         }
-        var description = new Mars.Core.ModelContainer.Entities.ModelDescription();
-        description.AddLayer<KruegerNationalPark.SavannaLayer>();
-        description.AddLayer<KruegerNationalPark.Precipitation>();
-        description.AddLayer<KruegerNationalPark.Temperature>();
-        description.AddAgent<KruegerNationalPark.Tree, KruegerNationalPark.SavannaLayer>();
-        var path = Path.Combine("..","src", "config.json");
-//            var path = args[0];
-        var config = SimulationConfig.Deserialize(path);
-        var task = Mars.Core.SimulationStarter.SimulationStarter.Start(description, config);
+        var description = new ModelDescription();
+        description.AddLayer<Precipitation>();
+        description.AddLayer<Temperature>();
+        description.AddLayer<SavannaLayer>();
+        description.AddLayer<FirewoodCollectorLayer>();
+        description.AddAgent<Tree, SavannaLayer>();
+        description.AddAgent<FirewoodCollector, FirewoodCollectorLayer>();
+        var stopwatch = Stopwatch.StartNew();
+       
+        var task = SimulationStarter.Start(description, args);
         var loopResults = task.Run();
-        System.Console.WriteLine($"Simulation execution finished after {loopResults.Iterations} steps");
+        Console.WriteLine($"Simulation execution finished after {loopResults.Iterations} steps in seconds: " + stopwatch.Elapsed.Seconds);
     }
 }
---
+++ model/Bushbuckridge/Interaction/ReplaningGoapInteraction.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Mars.Components.Services.Planning.Implementation;
+using Mars.Components.Services.Planning.Implementation.ActionCommons;
+using Mars.Components.Services.Planning.Interfaces;
+using Mars.Components.Services.Planning.Worldstate;
+using Mars.Interfaces.Agent;
+
+namespace Bushbuckridge.Agents
+{
+    public class ReplaningGoapInteraction : IInteraction
+    {
+        private readonly GoapPlanner _goapPlanner;
+        private readonly IGoapAgentStates _states;
+
+        public ReplaningGoapInteraction(GoapPlanner goapPlanner, IGoapAgentStates states)
+        {
+            _goapPlanner = goapPlanner;
+            _states = states;
+        }
+
+        public void Execute()
+        {
+            IList<IGoapAction> goapActions = new List<IGoapAction>();
+            do
+            {
+                goapActions = _goapPlanner.Plan();
+                var goal = _goapPlanner.SelectedGoal;
+                Console.WriteLine(goal + " " + goal.Relevance); //TODO delete
+                Print(_states.States.Values);
+
+                foreach (var action in goapActions)
+                {
+                    action.Execute();
+                }
+            } while (goapActions.Any() && !goapActions.First().Equals(AllGoalsSatisfiedAction.Instance));
+        }
+
+        private void Print(Dictionary<IGoapStateKey, GoapStateProperty>.ValueCollection items)
+        {
+            if (items != null)
+            {
+                foreach (var item in items)
+                {
+                    Console.Write(item + ", ");
+                }
+                Console.WriteLine("--");
+            }
+        }
+    }
+}
